28e691d51d7c7a80d393e127f91accac060ec78b
github-action-benchmark <github@users.noreply.github.com>
2021-09-13 03:51:15 +0000

    add Benchmark (go) benchmark result for 830f4f49c7497332644bf6e56393d15a25e79e0c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631504407955,
+  "lastUpdate": 1631505075253,
   "repoUrl": "https://github.com/ImVexed/gag",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,48 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkBigExample",
             "value": 20624789,
             "unit": "ns/op\t  247917 B/op\t    2900 allocs/op",
+            "extra": "58 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "v@nul.lu",
+            "name": "V-X",
+            "username": "ImVexed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "830f4f49c7497332644bf6e56393d15a25e79e0c",
+          "message": "Update benchmark.yml",
+          "timestamp": "2021-09-12T20:50:05-07:00",
+          "tree_id": "c74dad7135f72d3f9a083c4a0111c27f078390cc",
+          "url": "https://github.com/ImVexed/gag/commit/830f4f49c7497332644bf6e56393d15a25e79e0c"
+        },
+        "date": 1631505074802,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkExampleParallel",
+            "value": 1067,
+            "unit": "ns/op\t     760 B/op\t       5 allocs/op",
+            "extra": "970122 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkExample",
+            "value": 2060,
+            "unit": "ns/op\t     760 B/op\t       5 allocs/op",
+            "extra": "588566 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBigExample",
+            "value": 20663989,
+            "unit": "ns/op\t  247768 B/op\t    2900 allocs/op",
             "extra": "58 times\n2 procs"
           }
         ]
